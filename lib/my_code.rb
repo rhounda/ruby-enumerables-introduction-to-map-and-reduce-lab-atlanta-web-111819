bc89261e45111130a82e9cf54bc55442d7a1da5a
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-14 20:41:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # My Code here....
+def negativize 
+ array = [1, 2, 3, 4]
+ array.map{|n| n * 1}
+ 
+def no_change 
+  array = [1, 2, 3, 4]
+
+def double 
+ array = [1, 2, 3, 4]
+   array.map{|n| n * 2}
+   
+def square 
+ array = [1, 2, 3, 4]
+ arrqy.map{|n| n * n}
+ 
+
+def reduce_total
