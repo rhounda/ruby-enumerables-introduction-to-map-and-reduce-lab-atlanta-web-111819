98e8f535b399e7480a37d06450e95bf5a9d4d191
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-14 20:48:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # My Code here....
+def negativize 
+ array = [1, 2, 3, 4]
+ array.map{|n| n * 1}
+ 
+def no_change 
+  array = [1, 2, 3, 4]
+
+def double 
+ array = [1, 2, 3, 4]
+   array.map{|n| n * 2}
+   
+def square 
+ array = [1, 2, 3, 4]
+ array.map{|n| n * n}
+ 
+
+def reduce_total
+  array = [1, 2, 3,]
+  array.reduce(0) { |sum, n| sum + n }
+  
+  
+def all_true 
+  array = ["cat", "dog", "momkeys"]
+  array.reduce {|n| true }
+  
+def any_true 
+  array = ["car", "truck", "SUV"]
+  array.reduce {|n| true }
+    
+  
